1990e8e759419dd3f4f47670fbf1fa3f11bb70c7
ABDULLA MEHDI <s2305430@student.hb.se>
2025-01-22 17:12:16 +0100

    .

---
+++ MeTube.Data/ApplicationDbContext.cs
@@ -0,0 +1,51 @@
+﻿using MeTube.Data.Entity;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MeTube.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public virtual DbSet<User> Users { get; set; } = null!;
+        public virtual DbSet<Admin> Admins { get; set; } = null!;
+
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            // Configure User entity
+            modelBuilder.Entity<User>(entity =>
+            {
+                entity.HasKey(e => e.Id);
+                entity.Property(e => e.Username).ValueGeneratedOnAdd()
+                                                .IsRequired();
+            });
+
+            // Configure Admin entity
+            modelBuilder.Entity<Admin>(entity =>
+            {
+                entity.HasKey(e => e.Id);
+                entity.Property(e => e.Username).IsRequired().HasMaxLength(20);
+                entity.Property(e => e.Password).IsRequired().HasMaxLength(20);
+                entity.Property(e => e.Email).IsRequired();
+            });
+
+
+        }
+
+    }
+}
