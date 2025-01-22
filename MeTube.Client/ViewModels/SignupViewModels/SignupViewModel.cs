a57a3d31ba48b177dcc7481ebeeb3e36987c51b7
Oskar Andreasson <159390304+HissAnd@users.noreply.github.com>
2025-01-23 00:50:49 +0100

    Basic structure and functionality of signup viewmodel

---
+++ MeTube.Client/ViewModels/SignupViewModels/SignupViewModel.cs
@@ -0,0 +1,39 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using System.Text.RegularExpressions;
+
+namespace MeTube.Client.ViewModels.SignupViewModels
+{
+    public partial class SignupViewModel : ObservableObject
+    {
+        [ObservableProperty]
+        string username = string.Empty;
+
+        [ObservableProperty]
+        string email = string.Empty;
+
+        [ObservableProperty]
+        string password = string.Empty;
+
+        private static readonly Regex EmailRegex = new Regex(@"^[^@\s]+@[^@\s]+\.[^@\s]+$");
+
+        public SignupViewModel() { }
+
+        private bool ValidateFields()
+        {
+            if(!string.IsNullOrEmpty(Username) && !string.IsNullOrEmpty(Email) && EmailRegex.IsMatch(Email) && !string.IsNullOrEmpty(Password))
+                return true;
+            else
+                return false;
+        }
+
+        [RelayCommand]
+        public void SignupButton()
+        { 
+            bool isValid = ValidateFields();
+            //Check if user exist, else create user
+
+            //if(isValid && !userDontExist)
+        }
+    }
+}
