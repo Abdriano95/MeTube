1e5d34c4ce8eff2ec043044444e1c5625bc46bd9
Oskar Andreasson <159390304+HissAnd@users.noreply.github.com>
2025-01-24 21:10:26 +0100

    LoginForm in loginview with username and password completed, also added empty loginviewmodel

---
+++ MeTube.Client/ViewModels/LoginViewModels/LoginViewModel.cs
@@ -0,0 +1,19 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace MeTube.Client.ViewModels.LoginViewModels
+{
+    public partial class LoginViewModel : ObservableValidator
+    {
+        [ObservableProperty]
+        public string username = string.Empty;
+
+        [ObservableProperty]
+        public string password = string.Empty;
+        public LoginViewModel() { }
+
+        public async Task LoginButton()
+        {
+
+        }
+    }
+}
