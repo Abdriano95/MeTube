af170f65a8f7195f28949c72dfcc32047c73c8ff
Abdriano <s2305430@student.hb.se>
2025-02-08 22:19:02 +0100

    UserHistoryViewModel Created and tested (passed)

--- MeTube.Client/Models/History.cs
+++ MeTube.Client/Models/History.cs
@@ -2,7 +2,7 @@
 {
     public class History
     {
-        public int Id { get; set; }
+        public int VideoId { get; set; }
         public string VideoTitle { get; set; } = string.Empty;
         public DateTime DateWatched { get; set; }
     }
---
+++ MeTube.Client/ViewModels/HistoryViewModels/UserHistoryViewModel.cs
@@ -0,0 +1,53 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using MeTube.Client.Models;
+using MeTube.Client.Services;
+using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
+
+namespace MeTube.Client.ViewModels.HistoryViewModels
+{
+    [ObservableObject]
+    public partial class UserHistoryViewModel
+    {
+        private readonly IHistoryService _historyService;
+        private readonly NavigationManager _navigationManager;
+        private readonly IVideoService _videoService;
+        private IJSRuntime _jsRuntime;
+
+        [ObservableProperty]
+        private List<History> _userHistory = new();
+
+        public UserHistoryViewModel(IHistoryService historyService,
+            NavigationManager navigationManager, 
+            IVideoService videoService, 
+            IJSRuntime jsRuntime)
+        {
+            _historyService = historyService;
+            _navigationManager = navigationManager;
+            _videoService = videoService;
+            _jsRuntime = jsRuntime;
+        }
+
+        public async Task LoadUserHistoryAsync()
+        {
+            try
+            {
+                UserHistory.Clear();
+                var history = await _historyService.GetUserHistoryAsync();
+                if (history != null)
+                {
+                    foreach (var item in history.OrderByDescending(h => h.DateWatched))
+                    {
+                        UserHistory.Add(item);
+                    }
+                }
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Failed to load history. Please try again.");
+            }
+        }
+
+
+    }
+}
