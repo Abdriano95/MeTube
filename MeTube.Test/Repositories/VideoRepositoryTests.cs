b5e4c95d86446711aa14a2ac7475bf7964200f3c
RonnieSamson <166392656+RonnieSamson@users.noreply.github.com>
2025-02-10 15:10:14 +0100

    added GetallVideoTest

--- MeTube.Test/Repositories/VideoRepositoryTests.cs
+++ MeTube.Test/Repositories/VideoRepositoryTests.cs
@@ -19,6 +19,30 @@ namespace MeTube.Test.Repositories
             _mockUnitOfWork = new Mock<IUnitOfWork>();
             _mockUnitOfWork.Setup(uow => uow.Videos).Returns(_mockVideoRepo.Object);
         }
+        [Fact]
+        public async Task GetAllVideos_ShouldReturnListOfVideos()
+        {
+            // Arrange
+            var videos = new List<Video>
+            {
+                new Video
+                {
+                    Id = 1,
+                    UserId = 1,
+                    Title = "164. What is the Future of Blazor?",
+                    Description = "Should I learn a JavaScript framework or concentrate on mastering Blazor?",
+                    Genre = "Programming"
+                },
+                new Video
+                {
+                    Id = 2,
+                    UserId = 2,
+                    Title = "string",
+                    Description = "string",
+                    Genre = "string"
+                }
+            };
 
-    }  
+        }
+    }
 }
