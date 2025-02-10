d3b31b6287e7021b2ae316f2b9e62aecbca4c858
RonnieSamson <166392656+RonnieSamson@users.noreply.github.com>
2025-02-10 14:39:11 +0100

    added fluent assertions nuget package

--- MeTube.Test/Repositories/VideoRepositoryTests.cs
+++ MeTube.Test/Repositories/VideoRepositoryTests.cs
@@ -1,3 +1,24 @@
-﻿
+﻿using MeTube.Data.Entity;
+using MeTube.Data.Repository;
+using Moq;
+using Xunit;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using FluentAssertions;
 
- 
+namespace MeTube.Test.Repositories
+{
+    public class VideoRepositoryTests
+    {
+        private readonly Mock<IVideoRepository> _mockVideoRepo;
+        private readonly Mock<IUnitOfWork> _mockUnitOfWork;
+
+        public VideoRepositoryTests()
+        {
+            _mockVideoRepo = new Mock<IVideoRepository>();
+            _mockUnitOfWork = new Mock<IUnitOfWork>();
+            _mockUnitOfWork.Setup(uow => uow.Videos).Returns(_mockVideoRepo.Object);
+        }
+
+    }
+}
