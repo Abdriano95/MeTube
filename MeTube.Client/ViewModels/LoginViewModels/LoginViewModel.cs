121fc11efe4f99a718c563fce4959db9f1a7618b
Oskar Andreasson <159390304+HissAnd@users.noreply.github.com>
2025-02-12 12:54:39 +0100

    Logout, new viewmodel so it follows the SOLID principles

--- MeTube.Client/ViewModels/LoginViewModels/LoginViewModel.cs
+++ MeTube.Client/ViewModels/LoginViewModels/LoginViewModel.cs
@@ -77,21 +77,5 @@ namespace MeTube.Client.ViewModels.LoginViewModels
             Username = string.Empty;
             Password = string.Empty;
         }
-
-        public async Task Logout()
-        {
-            bool secureLogoff = await _jsRuntime.InvokeAsync<bool>("confirm", $"You sure you want to log-off?");
-            if (secureLogoff)
-            {
-                await _jsRuntime.InvokeVoidAsync("localStorage.removeItem", "jwtToken");
-                await _jsRuntime.InvokeVoidAsync("alert", "User loged-off!");
-                _navigation.NavigateTo("/login", forceLoad: true);
-            }
-            else
-            {
-                await _jsRuntime.InvokeVoidAsync("alert", "Unable to succesfully log-off user!");
-                _navigation.NavigateTo(_navigation.Uri, forceLoad: true);
-            }
-        }
     }
 }
