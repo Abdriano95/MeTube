5f7a6f6887257126d3a6b78c83a8755f2ad9f13e
RonnieSamson <166392656+RonnieSamson@users.noreply.github.com>
2025-02-12 13:39:47 +0100

    added constructor to VideoServiceTests

--- MeTube.Test/ClientServices/VideoServiceTests.cs
+++ MeTube.Test/ClientServices/VideoServiceTests.cs
@@ -1,12 +1,40 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using AutoMapper;
+using MeTube.Client.Models;
+using MeTube.Client.Services;
+using MeTube.DTO.VideoDTOs;
+using Microsoft.JSInterop;
+using Moq;
+using Moq.Protected;
+using System.Net;
+using System.Net.Http.Headers;
 using System.Text;
-using System.Threading.Tasks;
+using System.Text.Json;
+using Xunit;
 
 namespace MeTube.Test.ClientServices
 {
-    internal class VideoServiceTests
+    public class VideoServiceTests
     {
+        private readonly Mock<HttpMessageHandler> _mockHttpMessageHandler;
+        private readonly Mock<IMapper> _mockMapper;
+        private readonly Mock<IJSRuntime> _mockJsRuntime;
+        private readonly HttpClient _httpClient;
+        private readonly VideoService _videoService;
+
+        public VideoServiceTests()
+        {
+            _mockHttpMessageHandler = new Mock<HttpMessageHandler>();
+            _httpClient = new HttpClient(_mockHttpMessageHandler.Object);
+            _mockMapper = new Mock<IMapper>();
+            _mockJsRuntime = new Mock<IJSRuntime>();
+
+            _videoService = new VideoService(_httpClient, _mockMapper.Object, _mockJsRuntime.Object);
+
+            _mockJsRuntime
+                .Setup(x => x.InvokeAsync<string>(It.IsAny<string>(), It.IsAny<object[]>()))
+                .ReturnsAsync("fake-jwt-token");
+        }
     }
-}
+
+
+ }  
